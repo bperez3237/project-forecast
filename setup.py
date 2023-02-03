3663ad4f0328b44a903a352c1e10bff0d26d5429
brian <b.perez3237@gmail.com>
2023-02-02 20:05:38 -0500

    Import both databases from local files

---
+++ setup.py
@@ -0,0 +1,17 @@
+﻿from setuptools import setup
+
+setup(
+    name='project_name',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    author='Brian Perez',
+    author_email='b.perez3237@gmail.com',
+    url='https://github.com/bperez3237/project-forecast',
+    # packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    # install_requires=[
+    #     'dependency1',
+    #     'dependency2',
+    #     ...
+    # ],
+)
--- src/add_codes.py
+++ src/add_codes.py
@@ -1,1 +1,25 @@
-﻿
+﻿from numpy import number
+import xlsxwriter as xl
+import pandas as pd
+from datetime import date
+import pprint as pp
+from formats import *
+
+cost_rprt_xls = pd.ExcelFile(r'C:\Users\bperez\Iovino Enterprises, LLC\M007-NYCHA-Coney Island Sites - Documents\General\08 - BUDGET & COST\Cost Codes\Contract Forecasting Spreadsheet\Period 2 Export 02.02.23.xlsx')
+cost_rprt_df = pd.read_excel(cost_rprt_xls)
+
+sched_xls = pd.ExcelFile(r'C:\Users\bperez\Iovino Enterprises, LLC\M007-NYCHA-Coney Island Sites - Documents\General\08 - BUDGET & COST\Cost Codes\Schedule\M007 Billing Cost Schedule - 01.25.23.xlsx')
+sched_df = pd.read_excel(sched_xls, sheet_name='Cost Forecast')
+
+# print(cost_rprt_df.columns)
+# print(sched_df.columns)
+
+def add_codes(cost_report_df, schedule_df):
+    updated_report_df = cost_report_df
+    # for code in cost_report_df:
+    #     pass
+
+
+    return updated_report_df
+
+
