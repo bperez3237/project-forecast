75b81e8d8b11a62b86df0de6ef7181413a011442
brian <b.perez3237@gmail.com>
2023-02-03 22:03:25 -0500

    Take dataframe data and convert to dictionary

--- src/monthly_update.py
+++ src/monthly_update.py
@@ -17,4 +17,61 @@ billing_sched_df = pd.read_excel(sched_xls, sheet_name='Billing Forecast')
 activities_xls = pd.ExcelFile(r'C:\Users\bperez\Iovino Enterprises, LLC\M007-NYCHA-Coney Island Sites - Documents\General\27 - PERSONAL FOLDERS\Brian Perez\Coney Island\03 - Schedule\All Activities January 2023.xlsx')
 activities_df = pd.read_excel(activities_xls, sheet_name='TASK')
 
-print(activities_df)
+# print(activities_df.columns)
+# print(cost_sched_df.columns)
+# print(billing_sched_df.columns)
+
+# print(cost_sched_df['Category 1'].unique())
+# print(billing_sched_df['Category 1'].unique())
+# print(cost_sched_df['Category 1'].unique() + billing_sched_df['Category 1'].unique() + activities_df['Category'].unique())
+
+data_dic = {
+    'SITE': {
+            'ACTIVITIES': {
+
+            },
+            'BILLINGS': {
+
+            },
+            'COSTS': {
+
+            }
+    },
+    'NEW BUILDINGS': {
+            'ACTIVITIES': {
+
+            },
+            'BILLINGS': {
+
+            },
+            'COSTS': {
+                
+            }
+    },
+    'EXISTING BUILDINGS': {
+            'ACTIVITIES': {
+
+            },
+            'BILLINGS': {
+
+            },
+            'COSTS': {
+                
+            }
+    }
+}
+# print(activities_df.columns)
+for y in range(activities_df.shape[0]):
+    if activities_df['Activity Status'][y] != 'Completed' and activities_df['CODE TYPE'][y] == 'CONSTRUCTION':
+
+        if activities_df['Category'][y].upper() == 'SITE' or activities_df['Category'][y].upper() == 'NEW BUILDINGS' or activities_df['Category'][y].upper() == 'EXISTING BUILDINGS':
+            data_dic[activities_df['Category'][y].upper()]['ACTIVITIES'][activities_df['Activity ID'][y]] = {
+                'start_date': activities_df['(*)Start'][y],
+                'end_date': activities_df['(*)Finish'][y],
+                'activity_name': activities_df['Activity Name'][y],
+            }
+
+pp.pprint(data_dic)
+# turn pandas series into object
+def series_to_object(series):
+    return series.to_dict()
