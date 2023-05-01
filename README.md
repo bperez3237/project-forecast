5321c1cb300edce03a45eb729f61fdae6e2e7756
brian <b.perez3237@gmail.com>
2023-05-01 17:58:23 -0400

    Update db files

--- README.md
+++ README.md
@@ -36,17 +36,11 @@ Then, run the following command in your terminal to cache the data into pickle f
 python src/data/import_data.py
 ```
 
-python ./src/data/import_data.py
-
-```
-
 To write the app using the last imported data, run:
-```
 
+```python
 python ./src/app.py
-
 ```
-
 
 ## Contributing
 
@@ -57,4 +51,3 @@ Any contributions you make are greatly appreciated.
 3. Commit your Changes (git commit -m 'Add some AmazingFeature')
 4. Push to the Branch (git push origin feature/AmazingFeature)
 5. Open a Pull Request
-```
--- src/data/import_data.py
+++ src/data/import_data.py
@@ -3,15 +3,15 @@ import pickle
 
 
 print('start importing')
-cost_rprt_xls = pd.ExcelFile(r'C:\Users\bperez\Iovino Enterprises, LLC\M007-NYCHA-Coney Island Sites - Documents\General\08 - BUDGET & COST\Cost Codes\Contract Forecasting Spreadsheet\Period 3 Export 03.15.23.xlsx')
+cost_rprt_xls = pd.ExcelFile(r'C:\Users\bperez\Iovino Enterprises, LLC\M007-NYCHA-Coney Island Sites - Documents\General\08 - BUDGET & COST\Cost Codes\Contract Forecasting Spreadsheet\Period 4 Export 04.29.23.xlsx')
 cost_rprt_df = pd.read_excel(cost_rprt_xls)
 
-sched_xls = pd.ExcelFile(r'C:\Users\bperez\Iovino Enterprises, LLC\M007-NYCHA-Coney Island Sites - Documents\General\08 - BUDGET & COST\Cost Codes\Schedule\M007 Billing Cost Schedule - 03.14.23.xlsx')
+sched_xls = pd.ExcelFile(r'C:\Users\bperez\Iovino Enterprises, LLC\M007-NYCHA-Coney Island Sites - Documents\General\08 - BUDGET & COST\Cost Codes\Schedule\M007 Billing Cost Schedule - 03.15.23.xlsx')
 cost_sched_df = pd.read_excel(sched_xls, sheet_name='Cost Forecast')
 billing_sched_df = pd.read_excel(sched_xls, sheet_name='Billing Forecast')
 sub_cost_df = pd.read_excel(sched_xls, sheet_name='Sub Cost Forecast')
 
-activities_xls = pd.ExcelFile(r'C:\Users\bperez\Iovino Enterprises, LLC\M007-NYCHA-Coney Island Sites - Documents\General\27 - PERSONAL FOLDERS\Brian Perez\Coney Island\03 - Schedule\All Activities February 2023.xlsx')
+activities_xls = pd.ExcelFile(r'C:\Users\bperez\Iovino Enterprises, LLC\M007-NYCHA-Coney Island Sites - Documents\General\27 - PERSONAL FOLDERS\Brian Perez\Coney Island\03 - Schedule\Schedule March 2023.xlsx')
 activities_df = pd.read_excel(activities_xls, sheet_name='TASK')
 
 print('start saving')
